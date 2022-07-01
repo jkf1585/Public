577adee0594e88f33ff59f51cef546dae7cbc47c
jkf1585 <86493067+jkf1585@users.noreply.github.com>
2022-07-01 10:27:14 -0400

    Delete Azure_365_SetUS_SetUPNcnuedu.ps1

--- Azure_365_SetUS_SetUPNcnuedu.ps1
+++
@@ -1,6 +0,0 @@
-$username = "ADSyncService@cnu.edu"
-$password = ConvertTo-SecureString "803887bBf0!" -AsPlainText -Force
-$psCred = New-Object System.Management.Automation.PSCredential -ArgumentList ($username, $password)
-Connect-MSolService -Credential $psCred
-Get-MsolUser -All | Where-Object {$_.usagelocation -ne "US"} | Set-MsolUser -UsageLocation "US"
-Get-MsolUser -All | Where { -Not $_.UserPrincipalName.ToLower().StartsWith(“admin@”) } | ForEach { Set-MsolUserPrincipalName -ObjectId $_.ObjectId -NewUserPrincipalName ($_.UserPrincipalName.Split(“@”)[0] +  “@cnu.edu”) }
